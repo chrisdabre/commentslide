da7c154f480314d1d384b0481538c13dc759bc7d
Chris Dabre <chris.dabre12@gmail.com>
2025-04-20 16:18:35 -0700

    feat: add uuid dependency and sidebar menu constants
    Add the uuid package to generate unique IDs for sidebar menu items. Create a new file `menu.tsx` to define the sidebar menu structure using the uuid library for unique identifiers.

---
+++ src/constants/menu.tsx
@@ -0,0 +1,40 @@
+import { Activity, Contact, Home, Settings, SplinePointer } from 'lucide-react'
+import { v4 as uuid } from 'uuid'
+
+type FieldProps = {
+    label: string
+    id: string
+}
+
+type SidebarProps = {
+    icon: React.ReactNode
+} & FieldProps
+
+export const SIDEBAR_MENU: SidebarProps[] = [
+    {
+        id: uuid(),
+        label: "home",
+        icon: <Home />
+    },
+    {
+        id: uuid(),
+        label: "contacts",
+        icon: <Contact />
+    },
+    {
+        id: uuid(),
+        label: "automations",
+        icon: <Activity />
+    },
+    {
+        id: uuid(),
+        label: "integrations",
+        icon: <SplinePointer />
+    },
+    {
+        id: uuid(),
+        label: "settings",
+        icon: <Settings />
+    }
+
+]
