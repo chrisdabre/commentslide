2602538fb03388cb71c74829d266eaa27090c44a
Chris Dabre <chris.dabre12@gmail.com>
2025-04-20 14:23:28 -0700

    feat(dashboard): added the dashboard slug, loading,etc

---
+++ src/app/(protected)/dashboard/[slug]/layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {
+    children: React.ReactDOM
+}
+
+const Layout = (props: Props) => {
+  return (
+    <div>layout</div>
+  )
+}
+
+export default Layout
