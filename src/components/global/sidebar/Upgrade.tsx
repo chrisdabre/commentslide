ab3b39ffdc92c1b2197b0d721f772112346862c1
Chris Dabre <chris.dabre12@gmail.com>
2025-04-20 17:46:00 -0700

    feat(sidebar): add authentication, help, and upgrade components to sidebar
    Integrate ClerkAuthState for user authentication, add a help section, and include an UpgradeCard for subscription plans. This enhances the sidebar's functionality and user experience.

---
+++ src/components/global/sidebar/Upgrade.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UpgradeCard = () => {
+  return (
+    <div className='bg-[#252525] p-3 rounded-2xl flex flex-col gap-y-3'>
+        <span className='text-sm'>Upgrade to</span>
+    </div>
+  )
+}
+
+export default UpgradeCard
--- src/components/global/sidebar/index.tsx
+++ src/components/global/sidebar/index.tsx
@@ -4,6 +4,13 @@ import Logo from '@/components/ui/Logo'
 import { usePaths } from '@/hooks/use-nav'
 import React from 'react'
 import Items from './items'
+import { Separator } from '@/components/ui/separator'
+import { CircleHelpIcon } from 'lucide-react'
+import ClerkAuthState from '../clerk-auth-state'
+import { Button } from "@/components/ui/button"
+import { SubscriptionPlan } from '../subscription-plan'
+import UpgradeCard from './Upgrade'
+
 
 type Props = {
   slug: string
@@ -32,6 +39,33 @@ const Sidebar = ({slug}: Props) => {
           <div className='flex flex-col py-3'>
             <Items page={page} slug={slug}/>
         </div>
+        <div className='px-16'>
+          <Separator orientation="horizontal" className='bg-[#4d4d4d]' />
+        </div>
+        <div className='px-3 flex flex-col gap-y-5'>
+          <Button 
+            variant="ghost" 
+            className='flex gap-x-2 items-center justify-start w-full p-0'
+            onClick={() => {
+              const clerkButton = document.querySelector('[data-clerk-button]');
+              if (clerkButton instanceof HTMLElement) {
+                clerkButton.click();
+              }
+            }}
+          >
+            <ClerkAuthState />
+            <p className='text-[#9B9CA0]'>Profile</p>
+          </Button>
+          <div className='flex gap-x-3'>
+            <CircleHelpIcon />
+            <p className='text-[#9B9CA0]'>Help</p>
+          </div>
+        </div>
+        <SubscriptionPlan>
+          <div className='flex flex-1 flex-col justify-end'>
+            <UpgradeCard />
+          </div>
+        </SubscriptionPlan>
       </div>
     </div>
   )
