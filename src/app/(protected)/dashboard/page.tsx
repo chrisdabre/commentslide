20d22f383bbb76a6fbb1232c24edc7baca720757
Chris Dabre <chris.dabre12@gmail.com>
2025-04-23 22:49:20 -0700

    logic to redirect user if they have an account and !user, redirect to signin page

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,4 +1,5 @@
 import { onboardUser } from '@/actions/user'
+import { redirect } from 'next/navigation';
 import React from 'react'
 
 type Props = {}
@@ -7,9 +8,13 @@ const page = async (props: Props) => {
 
   const user = await onboardUser();
 
+  //If a user is created or there's an existing user, redirect to dashboard with their first and last name
+  if (user.status === 200 || user.status === 201) {
+    return redirect(`dashboard/${user.data?.firstname}${user.data?.lastname}`)
+  }
 
   return (
-    <div>Dashboard</div>
+    redirect('/sign-in')
   )
 }
 
