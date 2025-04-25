88241abc727158fc11f46a49b4cb26f31a213ae9
Chris Dabre <chris.dabre12@gmail.com>
2025-04-25 16:40:58 -0700

    mutation and optimistic ui

--- src/components/global/create-automation/index.tsx
+++ src/components/global/create-automation/index.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { Button } from '@/components/ui/button'
 import { Activity, Loader2 } from 'lucide-react'
 import React from 'react'
@@ -9,13 +11,13 @@ type Props = {}
 const CreateAutomation = (props: Props) => {
   //Todo: Create the automation in db using mutate
 
-  const {} = useCreateAutomation()
+  const { isPending, mutate } = useCreateAutomation()
     return (
         <Button className='lg:px-10 py-6 hover:opacity-80 text-white rounded-xl bg-gradient-to-br
              from-[#CC3804]
              font-medium
-             to-[#D064AC]'>
-            <Loader state={false} className="">
+             to-[#D064AC]' onClick={mutate}>
+            <Loader state={isPending} className="">
                 <Activity />
                 <p className='lg:inline hidden'>Create Automation</p>
             </Loader>
--- src/hooks/use-automations.ts
+++ src/hooks/use-automations.ts
@@ -1,6 +1,7 @@
 import { createAutomations } from "@/actions/automations"
 import { useMutationData } from "./use-mutaations-data"
 
+//we're giving it the keys, 'create-automation', function, and querykey
 export const useCreateAutomation = async () => {
     const { isPending, mutate } = useMutationData(
         ['create-automation'], 
