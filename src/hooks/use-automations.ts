6f3c87fbc3e0e6517cb4ba3b12ad81fd3cac69af
Chris Dabre <chris.dabre12@gmail.com>
2025-04-25 16:15:11 -0700

    more optimistic ui to invalidate cache etc

--- src/hooks/use-automations.ts
+++ src/hooks/use-automations.ts
@@ -1,5 +1,12 @@
+import { createAutomations } from "@/actions/automations"
 import { useMutationData } from "./use-mutaations-data"
 
 export const useCreateAutomation = async () => {
-    const {} = useMutationData()
+    const { isPending, mutate } = useMutationData(
+        ['create-automation'], 
+        () => createAutomations(),
+        'user-automations'
+    )
+
+    return { isPending, mutate }
 }
