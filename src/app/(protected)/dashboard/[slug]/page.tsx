8e6b989d97a5f96d3f6092a4b552eb462bada638
Chris Dabre <chris.dabre12@gmail.com>
2025-04-24 18:04:12 -0700

    data caching and optimistic ui using react query

--- src/app/(protected)/dashboard/[slug]/page.tsx
+++ src/app/(protected)/dashboard/[slug]/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const page = () => {
   return (
     <div className='flex lg:flex-row flex-col gap-5 w-full lg:w-10/12 xl:w-8/12 conteiner lg:pt-3'>
-      Test
+      Test Home
     </div>
   )
 }
