caa5f0985889673208daf02d7df15f267b2a63b9
Chris Dabre <chris.dabre12@gmail.com>
2025-04-25 17:33:09 -0700

    more mutations

--- src/components/global/create-automation/index.tsx
+++ src/components/global/create-automation/index.tsx
@@ -2,14 +2,19 @@
 
 import { Button } from '@/components/ui/button'
 import { Activity, Loader2 } from 'lucide-react'
-import React from 'react'
+import React, { useMemo } from 'react'
 import Loader from '../loader'
 import { useCreateAutomation } from '@/hooks/use-automations'
+import { v4 } from 'uuid'
 
 type Props = {}
 
 const CreateAutomation = (props: Props) => {
   //Todo: Create the automation in db using mutate
+
+  //Get the id of the newly created automation and store it in mutationId so we can store it in the db
+  //we use memo so that we can store the variable and not keep refreshing evetytime
+  const mutationId = useMemo(() => v4, []) //this will never refresh unless the dependency array [] changes
 
   /* @ts-ignore */
   const { isPending, mutate } = useCreateAutomation()
@@ -17,7 +22,8 @@ const CreateAutomation = (props: Props) => {
         <Button className='lg:px-10 py-6 hover:opacity-80 text-white rounded-xl bg-gradient-to-br
              from-[#CC3804]
              font-medium
-             to-[#D064AC]' onClick={mutate}>
+             to-[#D064AC]' onClick={() => mutate({name: 'Untitled', id: v4(), createdAt: new Date() })}>
+              {/* 4:39:58 */}
             <Loader state={isPending} className="">
                 <Activity />
                 <p className='lg:inline hidden'>Create Automation</p>
