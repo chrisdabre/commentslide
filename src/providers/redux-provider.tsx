57d6584e9cff31d279f76c3c6c16ad84257e982e
Chris Dabre <chris.dabre12@gmail.com>
2025-04-26 22:11:52 -0700

    more redux stuff

--- src/providers/redux-provider.tsx
+++ src/providers/redux-provider.tsx
@@ -1,11 +1,17 @@
+'use client'
+
+import { store } from '@/redux/store'
 import React from 'react'
+import { Provider } from 'react-redux'
 
-type Props = {}
-
-const ReduxProvider = (props: Props) => {
-  return (
-    <div>redux-provider</div>
-  )
+type Props = {
+    children: React.ReactNode
 }
 
-export default ReduxProvider
+const ReduxProvider = ({ children }: Props) => {
+  return (
+    <Provider store={store}>
+        {children}
+    </Provider>
+  )
+}
