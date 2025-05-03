a9436efb1ad662b3030f884fe804fc683c965675
Chris Dabre <chris.dabre12@gmail.com>
2025-05-03 12:52:28 -0700

    feat(instagram-callback): add loading and page components for Instagram callback
    Add loading component to display a spinner during the integration process and page component to handle Instagram callback logic, including redirecting to the dashboard upon successful integration.

---
+++ src/app/(protected)/callback/instagram/page.tsx
@@ -0,0 +1,28 @@
+import { redirect } from 'next/navigation'
+import React from 'react'
+
+type Props = {
+    searchaParams: {
+        code: string
+    }
+}
+
+//9:03:23
+//we're getting the seatchParams data from instagram
+const Page = async ( {searchaParams: { code }}: Props) => {
+
+    if (code) {
+        console.log(code)
+        const user = await onIntegrate(code.split('#_')[0])
+
+        if(user.status === 200) {
+            return redirect(`/dashboard/${user.data?.firstname}${user.data?.lastname}`)
+        } 
+    }
+
+  return (
+    <div>Page</div>
+  )
+}
+
+export default Page
