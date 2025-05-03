88198b17a7d9bc84ab22e696d02805c57e231d7e
Chris Dabre <chris.dabre12@gmail.com>
2025-05-02 17:59:01 -0700

    feat(integration-card): enable OAuth and disable button for active integration
    Add client-side functionality to handle Instagram OAuth and disable the connect button if the integration is already active. This ensures a better user experience by preventing duplicate integrations.

--- src/app/(protected)/dashboard/[slug]/integrations/_components/integration-card/page.tsx
+++ src/app/(protected)/dashboard/[slug]/integrations/_components/integration-card/page.tsx
@@ -1,5 +1,9 @@
+'use client'
+
 import { onOAuthInstagram } from '@/actions/integrations'
+import { onUserInfo } from '@/actions/user'
 import { Button } from '@/components/ui/button'
+import { useQuery } from '@tanstack/react-query'
 import React from 'react'
 
 type Props = {
@@ -14,6 +18,17 @@ const IntegrationCard = ( { title, description, icon, strategy }: Props) => {
   const onInstaOAuth = () => onOAuthInstagram(strategy)
 
 
+  //8:10:58
+  const { data } = useQuery({
+    queryKey: ['user-profile'],
+    queryFn: onUserInfo,
+  })
+
+  //to find if we have an active integratiion 
+  const integrated = data?.data?.Integrations.find(
+    (integration) => integration.name === strategy
+  )
+
   return (
       <div className='border-2 border-[#cc33ba] rounded-2xl gap-x-5 p-5 flex items-center justify-between'>
         {icon}
@@ -23,8 +38,8 @@ const IntegrationCard = ( { title, description, icon, strategy }: Props) => {
         </div>
 
         <Button 
-        //onClick={onInstaOAuth}
-        //disabled={integrated?.name === strategy}
+        onClick={onInstaOAuth}
+        disabled={integrated?.name === strategy}
         className='bg-gradient-to-br text-white rounded-full text-lg from-pink-500 font-medium to-violet-500 hover:opacity-70 transition duration=180'
         >
             Connect
